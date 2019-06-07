94556f17aaa4181e4f50fec6156982cbbf1e2b98
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-06-07 21:38:14 +0100

    changed sass to css modules updated webpack configs

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import cat from 'app/assets/random/nyancat.png'
 import { hot } from 'react-hot-loader'
+import styles from './styles.css'
 import Fetch from './fetch'
 
 const App = () => (
-  <div className='test'>
+  <div className={styles.app}>
     <h1>{process.env.baseUrl}</h1>
     <img src={cat}/>
     <Fetch/>
